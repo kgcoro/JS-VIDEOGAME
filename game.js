b6cff1c4d00549425999216dfa6883be99d63646
Kleber Coro <kgcoro@gmail.com>
2022-09-19 15:34:28 -0500

    funciones para movimiento del juego

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
@@ -39,10 +43,34 @@ function startGame() {
         });
     });
 
-   /*  for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowsCols[row-1][col-1]], (elementsSize * col), (elementsSize * row));
-            
-        }
-    } */
+   
+}
+
+window.addEventListener('keydown', moveByKeys)
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event) {
+    if (event.key == 'ArrowUp') moveUp();
+    else if (event.key == 'ArrowLeft') moveLeft();
+    else if (event.key == 'ArrowRight') moveRight();
+    else if (event.key == 'ArrowDown') moveDown();
+}
+
+function moveUp() {
+    console.log('Mover arriba');
+}
+
+function moveLeft() {
+    console.log('Mover izquierda');
+}
+
+function moveRight() {
+    console.log('Mover derecha');
+}
+
+function moveDown() {
+    console.log('Mover abajo');
 }
