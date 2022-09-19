936a8aef00ffd9e300db0d51341b64efe810add8
Kleber Coro <kgcoro@gmail.com>
2022-09-18 20:56:43 -0500

    Mejoramiento del codigo de renderizado

--- game.js
+++ game.js
@@ -1,14 +1,48 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-    // game.fillRect(0, 50, 100, 100);
-    // game.clearRect(0, 0, 50, 50);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-    game.font = '25px Verdana';
-    game.fillStyle = 'Purple';
+function setCanvasSize() {
+    (window.innerHeight > window.innerWidth) ? 
+    canvasSize = window.innerWidth * 0.8 : 
+    canvasSize = window.innerHeight * 0.8
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = (canvasSize / 10.6);
+
+    startGame();
+}
+
+function startGame() {      
+
+    
+    game.font = (elementsSize - 5) + 'px Verdana';
     game.textAlign = 'center';
-    game.fillText('Platzi', 25,25)
+
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowsCols = mapRows.map(row => row.trim().split(''));
+
+    mapRowsCols.forEach( (row, rowI) => {
+        row.forEach( (col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emoji, posX, posY);
+        });
+    });
+
+   /*  for (let row = 1; row <= 10; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowsCols[row-1][col-1]], (elementsSize * col), (elementsSize * row));
+            
+        }
+    } */
 }
